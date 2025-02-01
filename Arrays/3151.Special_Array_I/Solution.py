996e47cbb8aa5d0f423c09dbe1f810e1115780bd
Kummari Narendra <92687869+NarendraShalivahana@users.noreply.github.com>
2025-02-02 01:08:11 +0530

    Create Solution.py

---
+++ Arrays/3151.Special_Array_I/Solution.py
@@ -0,0 +1,12 @@
+3151.Special_Array_I
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i]%2==nums[i-1]%2){
+                return false;
+            }
+        }
+        return true; 
+    }
+}
